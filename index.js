9677de1fea1dac5d7562c403b169d40f1841a666
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-05-07 21:50:43 +0530

    mongoose schema and model added and test insert operation done

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 async function main() {
   await mongoose.connect(MONGO_URL);
@@ -14,6 +15,21 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root!");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My new villa",
+    description: "By the beach",
+    price: 1200,
+    location: "California",
+    country: "India",
+  });
+  await sampleListing
+    .save()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  res.send("successful testing");
 });
 
 app.listen(8080, () => {
---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default: "https://ibb.co/0t3gtwT",
+    set: (v) => (v === "" ? "https://ibb.co/0t3gtwT" : v),
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
