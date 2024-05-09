4fe6eef9cba9fe8f7fc9a4d06e335880a1ba5df8
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-05-09 18:01:34 +0530

    edit route creation done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+var methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,6 +18,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root!");
@@ -47,6 +49,20 @@ app.post("/listings", async (req, res) => {
   res.redirect("/listings");
 });
 
+//edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  console.log(listing);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 /* 
   let sampleListing = new Listing({
     title: "My new villa",
