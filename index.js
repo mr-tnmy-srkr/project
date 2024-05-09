4dce4b38248d8571f57432fd8be10e03df291ba2
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-05-09 17:26:57 +0530

    create a listing complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -13,13 +14,37 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hi, I am root!");
 });
 
-app.get("/listing", async (req, res) => {
+// Index Route
+app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("index.ejs", { allListings });
+  res.render("listings/index.ejs", { allListings });
+});
+
+//New Route
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 /* 
--- models/listing.js
+++ models/listing.js
@@ -7,10 +7,7 @@ const listingSchema = new Schema({
     required: true,
   },
   description: String,
-  image: {
-    // type: String,
-    // default: "https://ibb.co/0t3gtwT",
-    // set: (v) => (v === "" ? "https://ibb.co/0t3gtwT" : v),
+/*   image: {
     filename: {
       type: String,
       required: true,
@@ -22,6 +19,15 @@ const listingSchema = new Schema({
       default: "https://ibb.co/0t3gtwT",
       set: (v) => (v === "" ? "https://ibb.co/0t3gtwT" : v),
     },
+  }, */
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
   },
   price: Number,
   location: String,
