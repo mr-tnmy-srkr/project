6b9ae30a76b87ce69ad665c111f69001f4cdc887
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-05-15 23:25:44 +0530

    schema validation done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -28,6 +29,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) => {
   res.send("Hi, I am root!");
 });
+//validation for schema (Middleware)
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 
 // Index Route
 app.get(
@@ -56,10 +67,26 @@ app.get(
 //Create Route
 app.post(
   "/listings",
+  validateListing,
   wrapAsync(async (req, res, next) => {
-    if (!req.body.listing) {
+    /*  if (!req.body.listing) {
       throw new ExpressError(400, "Send valid data for listing");
     }
+    const newListing = new Listing(req.body.listing);
+    if (!newListing.title) {
+      throw new ExpressError(400, "Title is missing");
+    }
+    if (!newListing.description) {
+      throw new ExpressError(400, "Description is missing");
+    } */
+
+    // or
+    let result = listingSchema.validate(req.body);
+    // console.log(result);
+    if (result.error) {
+      throw new ExpressError(400, result.error);
+    }
+
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -80,6 +107,7 @@ app.get(
 //Update Route
 app.put(
   "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
     if (!req.body.listing) {
       throw new ExpressError(400, "Send valid data for listing");
